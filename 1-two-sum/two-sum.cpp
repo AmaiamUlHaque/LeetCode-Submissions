d6be50e62ce3b9e3783c11cb420e3d1ea4cd67ff
Amaiam Ul Haque <160980235+AmaiamUlHaque@users.noreply.github.com>
2025-12-24 05:08:10 -0500

    Time: 0 ms (100.00%) | Memory: 15 MB (19.93%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,14 +2,32 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        for  (int i=0; i<nums.size()-1; i++){
-            for (int j=i+1; j<nums.size(); j++){
-                if (nums[i]+nums[j] == target){
-                    return {i,j};
-                }
+        // for  (int i=0; i<nums.size()-1; i++){
+        //     for (int j=i+1; j<nums.size(); j++){
+        //         if (nums[i]+nums[j] == target){
+        //             return {i,j};
+        //         }
+        //     }
+        // }
+
+        // return {0}; //not possible
+
+
+        map <int, int> numMap;
+
+        int n = nums.size();
+        for (int i=0; i<n; i++){
+            int conjugate = target-nums[i];
+
+            if (numMap.count(conjugate)){
+                return {numMap[conjugate],i};
             }
+
+            numMap[nums[i]] = i;
         }
 
-        return {0};
+        return{0}; //not possible --> solution garunteed
+
     }
+
 };
